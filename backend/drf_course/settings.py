5acc89f739be83a65064616fa7eed64bcfff42df
danielrubens <danielrubensdaniel@gmail.com>
2023-01-17 02:21:48 -0300

    change secret_key, debug and allow_hosts

--- backend/drf_course/settings.py
+++ backend/drf_course/settings.py
@@ -6,17 +6,9 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g@alxoml0=u@c0^y=39y6^!b5$4^)zomuj5_5$+xbt-)12diq4'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = int(os.environ.get("DEBUG", default=0))
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
